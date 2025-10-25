db9bffcb91b5c729e8c711f2d669fc8ad0e4a5c9
Tejas <tejaskoliyoorvaranashi@gmail.com>
2025-10-25 21:31:15 +0200

    ci: ensure single final newline (fix flake8 W292)

--- src/experiments.py
+++ src/experiments.py
@@ -1,4 +1,4 @@
-﻿import hashlib
+import hashlib
 import math
 from typing import List, Dict
 
--- tests/test_experiment_basics.py
+++ tests/test_experiment_basics.py
@@ -1,4 +1,4 @@
-﻿from src.experiments import assign_arm, sample_size_two_props, summarize_churn
+from src.experiments import assign_arm, sample_size_two_props, summarize_churn
 
 
 def test_assign_arm_is_deterministic_and_balanced():
